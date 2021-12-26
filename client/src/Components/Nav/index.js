d8bd016e04113c7d195214595b33db5648001313
james940522 <James940522@gmail.com>
2021-12-26 23:19:29 +0900

    nav sub menu

--- client/src/Components/Nav/index.js
+++ client/src/Components/Nav/index.js
@@ -20,16 +20,34 @@ function Nav () {
                     회사소개
                 </span>
             </Link>
+            <ul>
+                <li>회사소개</li>
+                <li>협력사 소개</li>
+                <li>고객사 소개</li>
+                <li>오시는 길</li>
+            </ul>
             <Link to='/'>
                 <span>
                     사업소개
                 </span>
             </Link>
+            <ul>
+                <li>새벽배송</li>
+                <li>화물주선</li>
+                <li>풀필먼트</li>
+                <li>그로서리</li>
+            </ul>
             <Link to='/'>
                 <span>
                     공지사항
                 </span>
             </Link>
+            <ul>
+                <li>공지사항</li>
+                <li>인재채용</li>
+                <li>서비스 소개</li>
+                
+            </ul>
             <Link to='/'>
                 <span>
                     보도자료
