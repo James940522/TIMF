5374874d71341ae47c81d0cf099a28e26ad1bfef
james940522 <James940522@gmail.com>
2021-12-27 18:01:43 +0900

    feat: Notice&News

--- client/src/Components/Notice&News/index.js
+++ client/src/Components/Notice&News/index.js
@@ -1,16 +1,44 @@
+import { useNavigate } from "react-router-dom";
 import plusIcon from "../../assets/icon_plus.png";
 
 function NoticeNews() {
+  const navigate = useNavigate();
+
+  const onClickHandler = (e, key) => {
+    e.preventDefault();
+    navigate(`/${key}`);
+  };
+
   return (
     <>
       <h2>공지사항</h2>
       <div>
         <div>
           <h5>Notice</h5>
-          <img src={plusIcon} alt="더보기" />
+          <img
+            src={plusIcon}
+            alt="더보기"
+            onClick={(e) => {
+              onClickHandler(e, "notice");
+            }}
+          />
         </div>
       </div>
       <h2>보도자료</h2>
+      <div>
+        <div>
+          <h5>TimF News</h5>
+          <img
+            src={plusIcon}
+            alt="더보기"
+            onClick={(e) => {
+              onClickHandler(e, "reprot");
+            }}
+          />
+        </div>
+      </div>
     </>
   );
 }
+
+export default NoticeNews;
--- client/src/Pages/Main/index.js
+++ client/src/Pages/Main/index.js
@@ -2,12 +2,14 @@ import Intro from "../../Components/intro";
 import Business from "../../Components/Business";
 import Footer from "../../Components/Footer";
 import Connect from "../../Components/Connect";
+import NoticeNews from "../../Components/Notice&News";
 
 function Main() {
   return (
     <div>
       <Intro />
       <Business />
+      <NoticeNews />
       <Connect />
       <Footer />
     </div>
