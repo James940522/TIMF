bd4181153878e61e00c7f729b0eea3429ec34c4e
james940522 <James940522@gmail.com>
2021-12-28 23:51:10 +0900

    slide 수정

--- client/src/Components/intro/slide.js
+++ client/src/Components/intro/slide.js
@@ -6,39 +6,35 @@ import rightArrow from "../../assets/right-arrow.png";
 
 function Slide() {
   return (
-    <div
-      id="carouselExampleIndicators"
-      className="carousel slide"
-      data-ride="carousel"
-    >
-      <div className="carousel-inner">
-        <div className="carousel-item active carousel-item-left">
-          <img src={background1} alt="slide1" style={{ width: "100%" }} />
-          <div className="carousel-caption">
-            <h1 className="display-2">대한민국 No.1</h1>
+    <div>
+      <div>
+        <div>
+          <img src={background1} alt="slide1" width="100%" height="100%" />
+          <div>
+            <h1>대한민국 No.1</h1>
             <h3>Cold Chain Platform</h3>
           </div>
         </div>
-        <div className="carousel-item carousel-item-next carousel-item-left">
-          <img src={background2} alt="slide2" style={{ width: "100%" }} />
-          <div className="carousel-caption">
+        <div>
+          <img src={background2} alt="slide2" width="100%" height="100%" />
+          <div>
             <h1 className="display-2">Timf</h1>
             <h3>Connect Freshness</h3>
           </div>
         </div>
-        <div className="carousel-item">
-          <img src={background3} alt="slide3" style={{ width: "100%" }} />
-          <div className="carousel-caption">
-            <h1 className="display-2">Commerce Trend</h1>
+        <div>
+          <img src={background3} alt="slide3" width="100%" height="100%" />
+          <div>
+            <h1>Commerce Trend</h1>
             <h3>신선식품 물류를 선도하는 시스템 구축</h3>
           </div>
         </div>
       </div>
       <button>
-        <img src={leftArrow} alt="prev" />
+        <img src={leftArrow} alt="prev" width="20" height="20" />
       </button>
       <button>
-        <img src={rightArrow} alt="next" />
+        <img src={rightArrow} alt="next" width="20" height="20" />
       </button>
     </div>
   );
